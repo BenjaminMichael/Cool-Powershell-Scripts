867bbab3acb00428c402f4d25c860b07f9776a0c
Benjamin <benjaminriemersma@gmail.com>
2017-10-22 15:25:36 -0400

    Update ADGroupReportMOA.ps1

--- ADGroupReportMOA.ps1
+++ ADGroupReportMOA.ps1
@@ -1,9 +1,15 @@
-﻿$myOutput = @()
+#
+#get all the members of an ad group and get members of any subgroups
+#
+
+
+$distinguishedName = #paste an ad-group distinguished name here
+$myOutput = @()
 
 function nestedGroup($inputName){
 Get-ADGroupMember $inputName | ForEach-Object {$myOutput += "$($_.name) inherited from $($inputName)"}
 }
 
 
-Get-ADGroup -SearchBase 'OU=Museum of Art,OU=Organizations,OU=UMICH,DC=adsroot,DC=itcs,DC=umich,DC=edu' -filter * | ForEach-Object {$myOutput += "`n" + $_.name ;Get-ADGroupMember $_} | ForEach-Object{if($_.objectClass -eq 'user'){$myOutput += $_.name}else{write-host $_; nestedGroup($_)}}
+Get-ADGroup -SearchBase $distinguishedName -filter * | ForEach-Object {$myOutput += "`n" + $_.name ;Get-ADGroupMember $_} | ForEach-Object{if($_.objectClass -eq 'user'){$myOutput += $_.name}else{write-host $_; nestedGroup($_)}}
 $myOutput |  Out-File C:\temp\UMMAGroups.csv
