b71ffb351fa60039cec7c012f89ca89027be6502
Benjamin <benjaminriemersma@gmail.com>
2017-10-23 13:17:26 -0400

    Update ADGroupReportMOA.ps1

--- ADGroupReportMOA.ps1
+++ ADGroupReportMOA.ps1
@@ -1,9 +1,11 @@
-﻿$myOutput = @()
+$myDN = #enter the distinuished name for the AD grop you want to run this report against
+
+$myOutput = @()
 
 function nestedGroup($inputName){
 Get-ADGroupMember $inputName | ForEach-Object {$myOutput += "$($_.name) inherited from $($inputName)"}
 }
 
 
-Get-ADGroup -SearchBase 'OU=Museum of Art,OU=Organizations,OU=UMICH,DC=adsroot,DC=itcs,DC=umich,DC=edu' -filter * | ForEach-Object {$myOutput += "`n" + $_.name ;Get-ADGroupMember $_} | ForEach-Object{if($_.objectClass -eq 'user'){$myOutput += $_.name}else{write-host $_; nestedGroup($_)}}
+Get-ADGroup -SearchBase $myDN -filter * | ForEach-Object {$myOutput += "`n" + $_.name ;Get-ADGroupMember $_} | ForEach-Object{if($_.objectClass -eq 'user'){$myOutput += $_.name}else{write-host $_; nestedGroup($_)}}
 $myOutput |  Out-File C:\temp\UMMAGroups.csv
