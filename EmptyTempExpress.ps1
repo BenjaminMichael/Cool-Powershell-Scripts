2ae04bb17a74eb86d0965546f30e12e48dc436df
Benjamin <benjaminriemersma@gmail.com>
2017-10-23 13:14:46 -0400

    Update EmptyTempExpress.ps1

--- EmptyTempExpress.ps1
+++ EmptyTempExpress.ps1
@@ -1,4 +1,4 @@
-﻿$RegKeyPath = 'HKLM:\SOFTWARE\MiWorkspace\'
+$RegKeyPath = 'HKLM:\SOFTWARE\MiWorkspace\'
 $altRegKeyPath = 'HKLM:\SOFTWARE\WOW6432Node\MiWorkspace\'
 $value = "FINCore"
 $finCoreValueExist = $false
@@ -7,8 +7,13 @@ $altKey = Get-Item -LiteralPath $altRegKeyPath
 if ($Key.GetValue($Value) -ne $null){$finCoreValueExist = $true}else{if($altKey.GetValue($value) -ne $null){$finCoreValueExist = $true}}
 
 Function confirmPath($potentialTarget){
-if (test-path $potentialTarget){gci -Path $potentialTarget -Recurse -Filter * | %{if($_.LastWriteTime -lt (Get-Date).AddDays(-7)){secureDelete($_.VersionInfo.FileName)}}}
+if (test-path $potentialTarget){Get-ChildItem -Path $potentialTarget -Recurse -Filter * | ForEach-Object{if($_.LastWriteTime -lt (Get-Date).AddDays(-7)){secureDelete($_.VersionInfo.FileName)}}}
 }
+
+Function confirmDownloadsPath($potentialTarget){
+    if (test-path $potentialTarget){Get-ChildItem -Path $potentialTarget -Recurse -Filter * | ForEach-Object{secureDelete($_.VersionInfo.FileName)}}
+    }
+
 
 Function secureDelete($this){
 $myExpression= "C:\Windows\EUC\Bin\SDelete.exe -p 1 -s -q /accepteula '$($this)'"
@@ -22,5 +27,7 @@ confirmPath("$($env:USERPROFILE)\Local Settings\Temporary Internet Files\Content
 confirmPath($env:TEMP)
 
 if ($finCoreValueExist){
-confirmPath("$($env:USERPROFILE)\downloads\")
+confirmDownloadsPath("$($env:USERPROFILE)\downloads\")
+$Recycler = (New-Object -ComObject Shell.Application).NameSpace(0xa)
+$Recycler.items() | ForEach-Object { secureDelete($_.path) }
 }
