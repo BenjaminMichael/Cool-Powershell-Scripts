395e356cb0a125cdf5d62b460017bb8660399ea4
Benjamin <benjaminriemersma@gmail.com>
2017-11-03 15:36:46 -0400

    Update emptyTemp.ps1

--- emptyTemp.ps1
+++ emptyTemp.ps1
@@ -8,19 +8,22 @@ $value = "<# special value goes here.  this is set during the SCCM task sequence
 $specialValueExist = $false
 $key = Get-Item -LiteralPath $RegKeyPath
 $altKey = Get-Item -LiteralPath $altRegKeyPath
-if ($Key.GetValue($Value) -ne $null){$specialValueExist = $true}else{if($altKey.GetValue($value) -ne $null){$specialValueExist = $true}}
+
+if ($Key.GetValue($Value) -ne $null){
+    $specialValueExist = $true
+}
 
 Function confirmPath($potentialTarget){
-if (test-path $potentialTarget){Get-ChildItem -Path $potentialTarget -Recurse -Filter * | ForEach-Object{if($_.LastWriteTime -lt (Get-Date).AddDays(-7)){secureDelete($_.VersionInfo.FileName)}}}
+    if (test-path $potentialTarget){Get-ChildItem -Path $potentialTarget -Recurse -Filter * | ForEach-Object{if($_.LastWriteTime -lt (Get-Date).AddDays(-7)){secureDelete($_.VersionInfo.FileName)}}}
 }
 
 Function confirmDownloadsPath($potentialTarget){
     if (test-path $potentialTarget){Get-ChildItem -Path $potentialTarget -Recurse -Filter * | ForEach-Object{secureDelete($_.VersionInfo.FileName)}}
-    }
+ }
 
 Function secureDelete($this){
-$myExpression= "C:\Windows\<# special folder goes here.  this is part of the wim #>\Bin\SDelete.exe -p 1 -s -q /accepteula '$($this)'"
-Invoke-Expression "& $($myExpression)"
+    $myExpression= "C:\Windows\<# special folder goes here.  this is part of the wim #>\Bin\SDelete.exe -p 1 -s -q /accepteula '$($this)'"
+    Invoke-Expression "& $($myExpression)"
 }
 
 confirmPath("$($env:LOCALAPPDATA)\Microsoft\Windows\Temporary Internet Files\Content.Outlook\")
@@ -30,7 +33,7 @@ confirmPath("$($env:USERPROFILE)\Local Settings\Temporary Internet Files\Content
 confirmPath($env:TEMP)
 
 if ($specialValueExist){
-confirmDownloadsPath("$($env:USERPROFILE)\downloads\")
-$Recycler = (New-Object -ComObject Shell.Application).NameSpace(0xa)
-$Recycler.items() | ForEach-Object { secureDelete($_.path) }
+    confirmDownloadsPath("$($env:USERPROFILE)\downloads\")
+    $Recycler = (New-Object -ComObject Shell.Application).NameSpace(0xa) <# this is the really cool part #>
+    $Recycler.items() | ForEach-Object { secureDelete($_.path) }
 }
